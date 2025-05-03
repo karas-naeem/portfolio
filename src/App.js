752421eb42da75a45afd788392639acc68fc44b9
karas-naeem <karasnaeem1@gmail.com>
2025-05-03 16:05:01 +0300

    create a nav

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
-import { AppBar } from '@mui/material';
+import { AppBar,Avatar, Divider, Link, Stack } from '@mui/material';
 import './App.css';
+import {Routes,Route} from 'react-router-dom'
+import NotFound from './NotFound'
 
 function App() {
 
-  return (
-    <div className="App">
-      <AppBar>
-        
-      </AppBar>
-    </div>
-  );
+
+	return (
+		<div className="App">
+			<AppBar sx={{padding:"5px"}} color='transparent'>
+				<Stack direction="row" spacing={5}>
+					<Link href="https://github.com/karas-naeem">
+						<div title='my account at github'>
+							<Avatar alt='my logo on github' src='logo.png' sx={{border:"2.5px #AAA solid"}}/>
+						</div>
+					</Link>
+					<Stack direction="row" spacing={2} padding="2.5px">
+						<Divider orientation='vertical'/>
+						<Link underline='none' href="/">
+							home
+						</Link>
+						<Link underline='none' href="/roadmap">
+							roadmap
+						</Link>
+						<Link underline='none' href="/projects">
+							projects
+						</Link>
+					</Stack>
+				</Stack>
+				
+			</AppBar>
+			<Routes>
+				<Route index element={<></>}/>
+				<Route path="*" element={<NotFound/>} />
+			</Routes>
+		</div>
+	);
 }
+
 
 export default App;
---
+++ src/NotFound.js
@@ -0,0 +1,10 @@
+export default function NotFound()
+{
+	return (
+		<>
+			<h1>
+				404 | This page is not found
+			</h1>
+		</>
+	);
+}
